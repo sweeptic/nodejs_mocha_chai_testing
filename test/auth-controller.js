83ae1ddcd44757453ea5535a59c57b790d6ebf04
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-06 11:14:40 +0200

    471. testing async code

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -2,17 +2,31 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const User = require('../models/user');
 
-const authController = require('../controllers/auth');
+const AuthController = require('../controllers/auth');
 
 
 describe('auth controller login process.', () => {
 
-   it('should throw an error code 500 if accessing the database fails', () => {
+   it('should throw an error code 500 if accessing the database fails', (done) => {
 
       sinon.stub(User, 'findOne');
       User.findOne.throws();
 
-      expect(authController.login)
+      const req = {
+         body: {
+            email: 'test@test.com',
+            password: 'tester'
+         }
+      }
+
+
+      AuthController.login(req, {}, () => { }).then(result => {
+         // console.log(result);
+         expect(result).to.be.an('error')
+         expect(result).to.have.property('statusCode', 500)
+         //wait until execute
+         done()
+      })
 
       User.findOne.restore();
 
