ad815835cb2a4fd7b9d9c04cd3525de9e16a7dfc
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-06 10:34:28 +0200

    469. using stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const authMiddleware = require('../middleware/is-auth');
-
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 describe('auth middleware', () => {
 
@@ -25,6 +26,28 @@ describe('auth middleware', () => {
    });
 
 
+   it('should yield a userId after decoding the token', () => {
+      const req = {
+         get: function (headerName) {
+            return 'Bearer zyx';
+         }
+      };
+
+      //using stubs
+      sinon.stub(jwt, 'verify');
+      jwt.verify.returns({ userId: 'abc' }); //register function calls
+      //globally replace the verify method
+      // jwt.verify = function () {
+      //    return { userId: 'abc' }
+      // }
+      authMiddleware(req, {}, () => { });  //verify called here!
+      expect(req).to.have.property('userId');
+      expect(req).to.have.property('userId', 'abc');
+      expect(jwt.verify.called).to.be.true;
+      jwt.verify.restore();
+   });
+
+
    it('should throw an error if the token cannot be verified', () => {
       const req = {
          get: function () {
@@ -35,15 +58,6 @@ describe('auth middleware', () => {
    });
 
 
-   it('should yield a userId after decoding the token', () => {
-      const req = {
-         get: function () {
-            return 'Bearer zyx';
-         }
-      };
-      authMiddleware(req, {}, () => { });
-      expect(req).to.have.property('userId');
-   });
 
 });
 
