9a70d1f162184ff02ae4a3dd91b7fa3ab969af6b
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-06 09:41:37 +0200

    467. organizing multiple tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,12 +2,29 @@ const expect = require('chai').expect;
 const authMiddleware = require('../middleware/is-auth');
 
 
-it('should throw an error if not authentication header is present', () => {
-   const req = {
-      get: function () {
-         return null;
-      }
-   };
-   expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Not authenticated.')
+describe('auth middleware', () => {
+
+
+   it('should throw an error if not authentication header is present', () => {
+      const req = {
+         get: function () {
+            return null;
+         }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Not authenticated.')
+   });
+
+
+   it('should throw an error if the authentication header is only one string', () => {
+      const req = {
+         get: function () {
+            return 'zyx';
+         }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
+   });
+
+
 
 });
+
