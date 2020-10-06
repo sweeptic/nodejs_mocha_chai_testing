af2f5b92b90ee4ec3654869925e7f0c42023af37
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-06 11:32:56 +0200

    472. setting up a testing database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const User = require('../models/user');
-
+const mongoose = require('mongoose');
 const AuthController = require('../controllers/auth');
 
 
@@ -10,7 +10,7 @@ describe('auth controller login process.', () => {
    it('should throw an error code 500 if accessing the database fails', (done) => {
 
       sinon.stub(User, 'findOne');
-      User.findOne.throws();
+      User.findOne.throws();  // !!!
 
       const req = {
          body: {
@@ -19,7 +19,6 @@ describe('auth controller login process.', () => {
          }
       }
 
-
       AuthController.login(req, {}, () => { }).then(result => {
          // console.log(result);
          expect(result).to.be.an('error')
@@ -27,9 +26,32 @@ describe('auth controller login process.', () => {
          //wait until execute
          done()
       })
-
       User.findOne.restore();
+   });
+
+
+   it('should send a response with valid user status for existing user', (done) => {
+
+      const MONGODB_URI = 'mongodb+srv://olive4:hardfloor@nodejs.zzg9t.mongodb.net/test-nodejs_database?retryWrites=true&w=majority'
+
+
+      mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+         .then(result => {
+            const user = new User({
+               email: 'test@test.com',
+               password: 'tester',
+               name: 'Test',
+               posts: []
+            })
+            return user.save();
+         })
+         .then(() => {
+
+         })
+         .catch(err => console.log(err));
+
 
    });
+
 
 });
