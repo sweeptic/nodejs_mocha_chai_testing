8c77fc17ee1f763ff83654c3d990fc28152f233e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-06 12:44:23 +0200

    475. hooks

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,7 +5,30 @@ const mongoose = require('mongoose');
 const AuthController = require('../controllers/auth');
 
 
-describe('auth controller login process.', () => {
+describe('auth controller.', () => {
+
+   before((done) => {
+      const MONGODB_URI = 'mongodb+srv://olive4:hardfloor@nodejs.zzg9t.mongodb.net/test-nodejs_database?retryWrites=true&w=majority'
+      mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+         .then(result => {
+            const user = new User({
+               email: 'test@test.com',
+               password: 'tester',
+               name: 'Test',
+               posts: [],
+               _id: '5f79d8bf04810a29d879a1fe'
+            })
+            return user.save();
+         })
+         .then((res) => {
+            done()
+         })
+   })
+
+   beforeEach(() => { })
+   afterEach(() => { })
+
+
 
    it('should throw an error code 500 if accessing the database fails', (done) => {
 
@@ -32,45 +55,35 @@ describe('auth controller login process.', () => {
 
    it('should send a response with valid user status for existing user', (done) => {
 
-      const MONGODB_URI = 'mongodb+srv://olive4:hardfloor@nodejs.zzg9t.mongodb.net/test-nodejs_database?retryWrites=true&w=majority'
 
 
-      mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-         .then(result => {
-            const user = new User({
-               email: 'test@test.com',
-               password: 'tester',
-               name: 'Test',
-               posts: [],
-               _id: '5f79d8bf04810a29d879a1fe'
-            })
-            return user.save();
+      const req = { userId: '5f79d8bf04810a29d879a1fe' };
+      const res = {
+         statusCode: 500,
+         userStatus: null,
+         status: function (code) {
+            this.statusCode = code;
+            return this;
+         },
+         json: function (data) {
+            this.userStatus = data.status
+         }
+      }
+      AuthController.getUserStatus(req, res, () => { }).then(() => {
+         expect(res.statusCode).to.be.equal(200);
+         expect(res.userStatus).to.be.equal('I am new!');
+         done()
+      });
+   });
+
+   after((done) => {
+      User.deleteMany({})
+         .then(() => {
+            return mongoose.disconnect();
          })
          .then(() => {
-            const req = { userId: '5f79d8bf04810a29d879a1fe' };
-            const res = {
-               statusCode: 500,
-               userStatus: null,
-               status: function (code) {
-                  this.statusCode = code;
-                  return this;
-               },
-               json: function (data) {
-                  this.userStatus = data.status
-               }
-            }
-            AuthController.getUserStatus(req, res, () => { }).then(() => {
-               expect(res.statusCode).to.be.equal(200);
-               expect(res.userStatus).to.be.equal('I am new!');
-               User.deleteMany({})
-                  .then(() => {
-                     return mongoose.disconnect();
-                  })
-                  .then(() => {
-                     done();
-                  });
-            });
-         })
-         .catch(err => console.log(err));
-   });
+            done();
+         });
+   })
+
 })
